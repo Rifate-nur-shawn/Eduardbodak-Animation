c0f56380fcc542112eb1084b6255aba39db0d64b
iammahbubalam <malam212082@bscse.uiu.ac.bd>
2025-06-19 00:55:32 +0600

    Add Navbar component with styling

--- animation/src/Components/Navbar.jsx
+++ animation/src/Components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from 'react';
+import './Navbar.css';
 
-function Navbar() {
+const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <nav className="navbar">
+      <div className="navbar-container">
+        <div className="navbar-brand">
+          <h2>Animation</h2>
+        </div>
+        <ul className="navbar-menu">
+          <li className="navbar-item">
+            <a href="#home" className="navbar-link">Home</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#about" className="navbar-link">About</a>
+          </li>
+          <li className="navbar-item">
+            <a href="#contact" className="navbar-link">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
 
-export default Navbar
+export default Navbar;
